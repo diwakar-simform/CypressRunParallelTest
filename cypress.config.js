cb69ef5697cafeafa12ac8d673efd0fcc5f655a4
Diwakar <diwakar@simformsolutions.com>
2024-09-09 14:58:27 +0530

    added e2e.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'rc82w7',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
